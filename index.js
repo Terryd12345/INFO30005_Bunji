1347ed0cc92d43cf38e981787290c1edc09b2b71
Terryd12345 <terrencedenning@gmail.com>
2018-03-20 23:29:01 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("<h1>Welcome to Bunji</h1>");
+});
+
+const PORT = 5000;
+app.listen(PORT);
