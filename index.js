e154d8d7cfe5a8d1dabac0fbd8336d51ea46182e
Viska Tunru <viska.tunru@gmail.com>
2018-03-26 22:43:43 +1100

    modified home.ejs and index.ejs; added footer.ejs, head.ejs, header.ejs, and jsdefaults.ejs; added style.css; added countdown.js and main.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const router = require('./routes/routes');
+const router = require("./routes/routes");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+
+app.use(express.static("./public"));
 app.use(router);
 
 const PORT = process.env.PORT || 5000;
---
+++ public/js/countdown.js
@@ -0,0 +1,46 @@
+// Source: https://codepen.io/SitePoint/pen/MwNPVq
+
+function getTimeLeft(endTime) {
+    var t = Date.parse(endTime) - Date.parse(new Date());
+    
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var seconds = Math.floor((t / 1000) % 60);
+    
+    return {
+        "total": t,
+        "days": days,
+        "hours": hours,
+        "minutes": minutes,
+        "seconds": seconds
+    };
+}
+
+function startCountdown(id, endTime) {
+    var clock = document.getElementById(id);
+    
+    var days = clock.querySelector(".days");
+    var hours = clock.querySelector(".hours");
+    var minutes = clock.querySelector(".minutes");
+    var seconds = clock.querySelector(".seconds");
+    
+    function updateClock() {
+        var t = getTimeLeft(endTime);
+        
+        days.innerHTML = t.days;
+        hours.innerHTML = ("" + t.hours);
+        minutes.innerHTML = ("" + t.minutes);
+        seconds.innerHTML = ("" + t.seconds);
+        
+        if (t.total <= 0) {
+            clearInterval(interval);
+        }
+    }
+    
+    updateClock();
+    var interval = setInterval(updateClock, 1000);
+}
+
+var endTime = new Date(Date.parse("21 May 2018 22:00:00 GMT"));
+startCountdown("countdown", endTime);
