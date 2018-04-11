e981648af321be53bf14f8d5443c7d7d108b7be0
Terence Denning <tdenning@192-168-1-13.tpgi.com.au>
2018-04-12 00:11:48 +1000

    merge fix

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,8 @@
 const express = require('express');
-<<<<<<< HEAD
 const homeController = require('../controllers/homeController');
 const router = express.Router();
 
 router.get('/', homeController.getHome);
-=======
-const controller = require('../controller/homeController');
-const router = express.Router();
 
-router.get('/', controller.getHome);
->>>>>>> 05852b35db7422509c442ad856eaeb98321de500
 
 module.exports = router;
