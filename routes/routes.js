f7be65a177d44550c56052c13650fd10b2dd6dd8
Terence Denning <tdenning@192-168-1-13.tpgi.com.au>
2018-04-12 22:11:22 +1000

    fixed controller bug

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const homeController = require('../controller/controllers.js');
+const controllers = require('../controller/controllers.js');
 const router = express.Router();
 
 
