867c53585050eb63d6f09bb28058fbdb26706ec7
Terence Denning <tdenning@192-168-1-13.tpgi.com.au>
2018-04-14 10:21:05 +1000

    react refactor

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Header from './Header.js';
+import Header from './Header';
 
 const Dashboard = () => <h2>Dashboard</h2>
 const Skills = () => <h2>Skills</h2>
@@ -14,9 +14,11 @@ class App extends Component {
         <BrowserRouter>
           <div>
             <Header />
-            <Route exact path='/' component={Landing} />
-            <Route exact path='/dashboard' component={Dashboard} />
-            <Route exact path='/skills' component={Skills} />
+            <div className="content">
+              <Route exact path='/' component={Landing} />
+              <Route exact path='/dashboard' component={Dashboard} />
+              <Route exact path='/skills' component={Skills} />
+            </div>
           </div>
         </BrowserRouter>
       </div>
