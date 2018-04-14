1aaaafb986c0bf83d7985c44fae16870eeeb1c7b
Terence Denning <tdenning@192-168-1-13.tpgi.com.au>
2018-04-14 22:53:02 +1000

    moved dashboard from server to client

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -5,7 +5,11 @@ export default class Dashboard extends Component {
   render() {
     return (
       <div className="dashboard">
-
+          <div className='profile'>Profile</div>
+          <div className='awards'>Awards</div>
+          <div className='contact'>contact</div>
+          <div className='events'>events</div>
+          <div className='recommendations'>Recommended</div>
       </div>
     );
   }
