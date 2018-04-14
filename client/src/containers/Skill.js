43c84431077bdf54a6433edc383db376101bb0e5
Terence Denning <tdenning@192-168-1-13.tpgi.com.au>
2018-04-15 03:37:16 +1000

    added selection state to users

--- client/src/containers/Skill.js
+++ client/src/containers/Skill.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
 
 class Skill extends Component {
-    constructor(props){
-        super(props);
-        this.state = { isSelected: false };
-    }
-
-    onSelected(event){
-        if(this.state.isSelected === false){
-          this.setState({ isSelected: true });
-        } else {
-          this.setState({ isSelected: false });
-        }
-    }  
+    
 
   render() {
     return (
-      <div className="skill">
-      <div className="skills-panel centered">
-          Facebook
-      </div>
+      <div className="skill"  onClick='alert("hi")'>
+          <div
+          className="skills-panel centered">
+              Facebook
+          </div>
       </div>
     );
   }
--- client/src/containers/User.js
+++ client/src/containers/User.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 
 export default class User extends Component {
+    constructor(props){
+        super(props);
+        this.state = { isSelected: false, color:'#ccc' };
+        this.onSelected = this.onSelected.bind(this);
+    }
 
-  render() {
-    return (
-      <div className="user">
-          <div className="mentor-panel">
-              <div className="mentor-pic">
-                  <img src={require("../images/user.png")} />
-              </div>
-              <div className="mentor-desc">
-                  <p>blah blah blah</p>
-              </div>
-          </div>
-      </div>
-    );
-  }
+    onSelected(event) {
+        if(this.state.isSelected === false){
+          this.setState({ isSelected: true, color:'lightblue' });
+        } else {
+          this.setState({ isSelected: false, color:'#ccc' });
+        }
+    }
+
+    render() {
+      return (
+        <div className="user"
+        onClick={this.onSelected}
+        >
+            <div className="mentor-panel"
+            style={{backgroundColor:this.state.color}}>
+                <div className="mentor-pic">
+                    <img src={require("../images/user.png")} />
+                </div>
+                <div className="mentor-desc">
+                    <p>blah blah blah</p>
+                </div>
+            </div>
+        </div>
+      );
+    }
 }
