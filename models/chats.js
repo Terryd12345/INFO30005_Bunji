1ce7d3e7498851d6438174ae3c45e2a833ec6ec3
omjadas <omja.das@gmail.com>
2018-04-14 11:16:52 +1000

    template for relationships page, simple chat data

---
+++ models/chats.js
@@ -0,0 +1,26 @@
+module.exports.chats = [
+    {
+        user: "john doe",
+        messages: [
+            {
+                date: date(13),
+                sender: "",
+                message: "hey"
+            },
+            {
+                date: date(),
+                message: ""
+            }
+        ]
+    },
+    {
+        user: "jane doe",
+        messages: [
+            {
+                date: date(),
+                sender: "",
+                message: ""
+            }
+        ]
+    }
+];
