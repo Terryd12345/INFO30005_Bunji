8c96270cb2e7d73185bbd7ccda9bf12740d57102
Viska Tunru <viska.tunru@gmail.com>
2018-04-14 22:17:11 +1000

    created arrays for skills, mentees, and mentors; used dummy data in selectSkills.ejs and findMentor.ejs

--- controller/controllers.js
+++ controller/controllers.js
@@ -1,19 +1,26 @@
+const skills = require("../models/skills.js");
+const mentors = require("../models/mentors.js");
+
 module.exports.getHome = (req, res) => {
-  res.render("home");
+    res.render("home");
 };
 
 module.exports.getSelectSkills = (req, res) => {
-  res.render("selectSkills");
+    res.render("selectSkills", {
+        skills: skills
+    });
 };
 
 module.exports.getFindMentor = (req, res) => {
-  res.render("findMentor");
+    res.render("findMentor", {
+        mentors: mentors
+    });
 };
 
 module.exports.getDashboard = (req, res) => {
-  res.render("dashboard");
+    res.render("dashboard");
 };
 
 module.exports.getProfile = (req, res) => {
-  res.render("profile");
+    res.render("profile");
 };
