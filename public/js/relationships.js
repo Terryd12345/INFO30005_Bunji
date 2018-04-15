4040d52dd64bb5792f1d9cbf8d18aafd2ef04630
omjadas <omja.das@gmail.com>
2018-04-15 17:42:46 +1000

    added support to switch between chats

--- public/js/relationships.js
+++ public/js/relationships.js
@@ -1,3 +1,5 @@
+var currChat
+
 function addMessage(message, self) {
     var messages = document.querySelector("#messages");
     var newMessage = document.createElement("div");
@@ -13,12 +15,39 @@ function addMessage(message, self) {
 function readMessage() {
     var message = document.querySelector("#message").value;
     addMessage(message, true);
-};
-
-function loadMessages(chat) {
-    chat.messages.forEach(element => {
-        element.sender == chat.connection ? addMessage(element.message, false) : addMessage(element.message, true)
+    document.querySelector("#message").value = "";
+    chats[currChat].messages.push({
+        date: new Date(),
+        sender: "Omja Das",
+        message: message
     });
 };
 
+function loadMessages(chat) {
+    console.log(chat);
+    document.querySelector("#messages").innerHTML = "";
+    chat.messages.forEach(element => {
+        element.sender == chat.connection ? addMessage(element.message, false) : addMessage(element.message, true)
+    });
+    currChat = chat.chatID;
+};
+
+function loadChats(chats) {
+    chats.forEach(chat => {
+        var connections = document.querySelector("#connections");
+        var connection = document.createElement("div");
+        connection.className = "connection";
+        connection.appendChild(document.createTextNode(chat.connection));
+        connection.setAttribute("data-chatID", chat.chatID);
+        connection.setAttribute("onclick", `loadChat(${chat.chatID})`);
+        connections.appendChild(connection);
+    });
+};
+
+function loadChat(chatID) {
+    loadMessages(chats[chatID]);
+};
+
+
+loadChats(chats);
 loadMessages(chats[0]);
