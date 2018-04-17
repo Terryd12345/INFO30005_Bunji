8345f2ecdf0d196e744d9fe2340504112fa4f5a5
omjadas <omja.das@gmail.com>
2018-04-17 18:22:09 +1000

    updated MessageBox and index.css

--- client/src/containers/MessageBox.js
+++ client/src/containers/MessageBox.js
@@ -15,11 +15,9 @@ class MessageBox extends Component {
 
     render() {
         return (
-            <div className="messageBox">
-            <div id="messageBox">
-                <textarea id="message"></textarea>
+            <div id="messageBox" action="">
+                <input id="message" autocomplete="off" type="text" placeholder="Message..." aria-label="Message..." onkeydown="if (event.keyCode == 13)document.getElementById('send').click()" />
                 <button id="send" onClick="readMessage()">Send</button>
-            </div>
             </div>
         );
     }
