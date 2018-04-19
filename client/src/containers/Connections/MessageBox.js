e9e18e481841aeeac829c8793a01d0f76dd49725
omjadas <omja.das@gmail.com>
2018-04-20 00:00:44 +1000

    removed legacy code

--- client/src/containers/Connections/MessageBox.js
+++ client/src/containers/Connections/MessageBox.js
@@ -7,28 +7,6 @@ class MessageBox extends Component {
         this.readMessage = this.readMessage.bind(this);
         this.addMessage = this.addMessage.bind(this);
     }
-    readMessage() {
-        var message = document.querySelector("#message").value;
-        this.addMessage(message, true);
-        document.querySelector("#message").value = "";
-        // chats[currChat].messages.push({
-        //     date: new Date(),
-        //     sender: "Omja Das",
-        //     message: message
-        // });
-    };
-
-    addMessage(message, self) {
-        var messages = document.querySelector("#messages");
-        var newMessage = document.createElement("div");
-        var newMessageContent = document.createTextNode(message);
-    
-        newMessage.appendChild(newMessageContent);
-        newMessage.className = self ? "message own" : "message other";
-    
-        messages.appendChild(newMessage);
-        messages.scrollTop = messages.scrollHeight;
-    };
 
     render() {
         return (
