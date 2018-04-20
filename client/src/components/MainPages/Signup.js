8b57750c29b363de0311d7fa304a036955a69e5d
Terence Denning <tdenning@192-168-1-13.tpgi.com.au>
2018-04-20 21:31:58 +1000

    facebook login

--- client/src/components/MainPages/Signup.js
+++ client/src/components/MainPages/Signup.js
@@ -1,26 +1,35 @@
 import React, { Component } from "react";
 import { GoogleLogin } from 'react-google-login';
+import FacebookLogin from 'react-facebook-login';
 import Keys from '../../keys';
 
 class SignUp extends Component {
-
-    constructor(props) {
-        super(props);
-        
-    }
 
     render() {
         const responseGoogle = (response) => {
             console.log(response);
         }
+        const responseFacebook = (response) => {
+            console.log(response);
+        }
+        
         return (
             <div id="signup">
                 <GoogleLogin
-                    clientId={Keys.ClientID}
+                    clientId={Keys.GoogleClientID}
                     buttonText="Login with Google"
                     onSuccess={responseGoogle}
                     onFailure={responseGoogle}
                 />
+                <FacebookLogin
+                    appId={Keys.FacebookID}
+                    autoLoad
+                    callback={responseFacebook}
+                    render={renderProps => (
+                        <button onClick={renderProps.onClick}>This is my custom FB button</button>
+                    )}
+                />
+                
             </div>
         );
     };
