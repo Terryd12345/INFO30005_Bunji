16b97e1608aa8a43d88523ea66ecc4dd34603d49
Terence Denning <tdenning@192-168-1-13.tpgi.com.au>
2018-04-20 20:08:17 +1000

    reinitialised

--- client/src/keys.js
+++ client/src/keys.js
@@ -1,5 +1,5 @@
 
 var Keys = module.exports = {
     ClientID: "820164325496-e082jen2k1t4c28n98716nlmjkddp6og.apps.googleusercontent.com",
-    ClientSecret: "34PcJs9WfM819LWjLPTBYTeh"
+    ClientSecret: "QYIJXnjnvlwxZ-AsRJz7crtD"
 }
