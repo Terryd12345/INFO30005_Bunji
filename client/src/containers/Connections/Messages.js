d2bd3ea0dde6df76ab0389e84a680422650de25d
omjadas <omja.das@gmail.com>
2018-04-20 13:07:18 +1000

    added autoscrolling

--- client/src/containers/Connections/Messages.js
+++ client/src/containers/Connections/Messages.js
@@ -3,12 +3,28 @@ import React, { Component } from 'react';
 class Messages extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {};
+
+        this.componentDidMount = this.componentDidMount.bind(this);
+        this.componentDidUpdate = this.componentDidUpdate.bind(this);
+        this.scrollToBotton = this.scrollToBotton.bind(this);
+    }
+
+    componentDidMount() {
+        this.scrollToBotton();
+    }
+
+    componentDidUpdate() {
+        this.scrollToBotton();
+    }
+
+    scrollToBotton() {
+        this.myRef.scrollTop = this.myRef.scrollHeight;
     }
 
     render() {
         return (
-            <div id="messages">
+            <div ref={(el) => { this.myRef = el; }} id="messages">
                 {this.props.chat.messages.map(message => {
                     if (message.sender === this.props.chat.user1) {
                         return <div className="message own">{message.message}</div>;
