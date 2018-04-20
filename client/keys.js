fac17d5e8b8f975aed56b5eaeb2d3dfa9cc5f471
Terence Denning <tdenning@192-168-1-13.tpgi.com.au>
2018-04-20 22:08:54 +1000

    credential changes

---
+++ client/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    FacebookID = '425379877913502',
+    GoogleClientID = '820164325496-e082jen2k1t4c28n98716nlmjkddp6og.apps.googleusercontent.com',
+    GoogleSecretID = 'yMoaMx4SzN-stz0n5BugXzcl'
+};
