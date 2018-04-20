dc23cbc608aa0ef33d4e3621c17cd9705a4af1ca
Terence Denning <tdenning@192-168-1-13.tpgi.com.au>
2018-04-20 22:12:03 +1000

    removed tracking

--- client/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    FacebookID = '425379877913502',
-    GoogleClientID = '820164325496-e082jen2k1t4c28n98716nlmjkddp6og.apps.googleusercontent.com',
-    GoogleSecretID = 'yMoaMx4SzN-stz0n5BugXzcl'
-};
