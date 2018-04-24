472ef8618d655a1c589f80865c94e0e56cd901cf
omjadas <omja.das@gmail.com>
2018-04-24 13:41:21 +1000

    added route handling in server.js

--- server/server.js
+++ server/server.js
@@ -13,15 +13,24 @@ const staticFiles = express.static(path.join(__dirname, "../../client/build"))
 app.use(staticFiles)
 
 router.get("/cities", (req, res) => {
-  const cities = [
-    {name: "New York City", population: 8175133},
-    {name: "Los Angeles",   population: 3792621},
-    {name: "Chicago",       population: 2695598}
-  ]
-  res.json(cities)
+    const cities = [
+        {name: "New York City", population: 8175133},
+        {name: "Los Angeles",   population: 3792621},
+        {name: "Chicago",       population: 2695598}
+    ]
+    res.json(cities)
 })
+
+router.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'), function(err) {
+        if (err) {
+            res.status(500).send(err);
+        };
+    });
+})
+
 app.use(router)
 app.set("port", (process.env.PORT || 5000))
 app.listen(app.get("port"), () => {
-  console.log(`Listening on ${app.get("port")}`)
+    console.log(`Listening on ${app.get("port")}`)
 })
