e6922ecaf3064f5e19e39bd85c2cb4b78480c860
Terence Denning <tdenning@192-168-1-13.tpgi.com.au>
2018-04-27 20:29:52 +1000

    auth bug fix

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
-import bodyParser from "body-parser";
-import express from "express";
+import bodyParser from "body-parser"
+import express from "express"
 import path from "path";
-<<<<<<< HEAD
 import keys from './keys.js';
 
 const app = express()
@@ -10,29 +9,23 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 app.use(cors());
-=======
-import api from "../routes/api";
 
-const app = express();
->>>>>>> 03dae7c44b8101f018b91b8300bf81694cef5afe
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+const router = express.Router()
+const staticFiles = express.static(path.join(__dirname, "../../client/build"))
 
-const router = express.Router();
-const staticFiles = express.static(path.join(__dirname, "../../client/build"));
-
-app.use(staticFiles);
-app.use("/api", api);
+app.use(staticFiles)
 
 router.get("/cities", (req, res) => {
     const cities = [
         {name: "New York City", population: 8175133},
         {name: "Los Angeles",   population: 3792621},
         {name: "Chicago",       population: 2695598}
-    ];
-    res.json(cities);
-});
+    ]
+    res.json(cities)
+})
 
 router.post("/api/login", (req, res) => {
 
@@ -64,6 +57,7 @@ router.get(
     passport.authenticate('google')
 );
 
+
 // END AUTHENTICATION -----------------------------------------------
 
 router.get("/*", (req, res) => {
@@ -74,8 +68,8 @@ router.get("/*", (req, res) => {
     });
 })
 
-app.use(router);
-app.set("port", (process.env.PORT || 5000));
+app.use(router)
+app.set("port", (process.env.PORT || 5000))
 app.listen(app.get("port"), () => {
-    console.log(`Listening on ${app.get("port")}`);
+    console.log(`Listening on ${app.get("port")}`)
 })
