754db4cea5ba61f1cc4f4132fb97d4551c9d4262
Viska Tunru <viska.tunru@gmail.com>
2018-04-27 22:31:26 +1000

    added state to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,17 @@ import React, { Component } from "react";
 import SignUp from "./SignUp/SignUp";
 
 class Header extends Component {
+    constructor(props, context) {
+        super(props, context);
+        
+        this.showRegister = this.showRegister.bind(this);
+        this.showLogin = this.showLogin.bind(this);
+        
+        this.state = {
+            loggedIn: false
+        };
+    }
+    
     showRegister = () => {
         this.signup.showRegister();
     }
@@ -14,21 +25,30 @@ class Header extends Component {
         return (
             <div className="navbar">
                 <SignUp ref={signup => this.signup = signup} />
-                
-                <ul>
-                    <li id="logo"><a href="/"><img src={require("../images/logo.jpg")} alt="Logo" /></a></li>
-                    <li id="dropdown">
-                        &#9776;
-                        <div id="dropdown-content">
-                            <a href="/dashboard">Dashboard</a>
-                            <a href="/profile">Profile</a>
-                            <a href="/connections">Connections</a>
-                            <a href="/">Log Out</a>
-                        </div>
-                    </li>
-                    <li><a onClick={this.showLogin.bind(this)}>Log In</a></li>
-                    <li><a onClick={this.showRegister.bind(this)}>Register</a></li>
-                </ul>
+    
+                {
+                    this.state.loggedIn ? (
+                        <ul>
+                            <li id="dropdown">
+                                &#9776;
+                                <div id="dropdown-content">
+                                    <a href="/dashboard">Dashboard</a>
+                                    <a href="/connections">Connections</a>
+                                    <a href="/">Log Out</a>
+                                </div>
+                            </li>
+                            <li><a href="/">Log Out</a></li>
+                            <li><a href="/connections">Connections</a></li>
+                            <li><a href="/dashboard">Dashboard</a></li>
+                        </ul>
+                    ) : (
+                        <ul>
+                            <li id="logo"><a href="/"><img src={require("../images/logo.jpg")} alt="Logo" /></a></li>
+                            <li><a onClick={this.showLogin}>Log In</a></li>
+                            <li> < a onClick = {this.showRegister}>Register</a></li>
+                        </ul>
+                    )
+                }
             </div>
         );
     }
