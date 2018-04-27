56fdc37ffe29a989d56022ff7043af685e324fa9
Terence Denning <tdenning@192-168-1-13.tpgi.com.au>
2018-04-27 20:31:42 +1000

    api call display

--- client/src/utils/getCityData.js
+++ client/src/utils/getCityData.js
@@ -5,6 +5,6 @@ const BASE_URL = "http://localhost:5000";
 export { getCityData }
 
 function getCityData(){
-    const url = "/cities";
+    const url = `${BASE_URL}/cities`;
     return axios.get(url).then(response => response.data);
 }
