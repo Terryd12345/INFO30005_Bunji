a63dc2935fda39bf3921e9947aaf39dec0f7575f
omjadas <omja.das@gmail.com>
2018-04-27 22:19:48 +1000

    removed citydata

--- client/src/components/MainPages/Signup.js
+++ client/src/components/MainPages/Signup.js
@@ -10,23 +10,13 @@ class SignUp extends Component {
     };
 
     componentDidMount(){
-        getCityData().then((cityData) => {
-            this.setState({ cityData} );
-        });
     }
 
     render() {
         return (
             <div className="wrapper" id="login">
                 <div id="signup">
-
                     <h1>Login</h1>
-
-
-                </div>
-                <div className="city-data">
-                    { console.log(this.state.cityData) }
-                    <p>{this.state.cityData.map(city => city.name)}</p>
                 </div>
             </div>
         );
