18004e11838a1771defd20a6c417e10c6614e024
omjadas <omja.das@gmail.com>
2018-04-27 22:52:49 +1000

    fixed indentation in passport.js

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,13 +3,13 @@ import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { auth } from "../config";
 
 passport.use(new GoogleStrategy({
-  clientID: auth.GOOGLE_CLIENT_ID,
-  clientSecret: auth.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+    clientID: auth.GOOGLE_CLIENT_ID,
+    clientSecret: auth.GOOGLE_CLIENT_SECRET,
+    callbackURL: "/auth/google/callback"
 },
-  (accessToken, refreshToken, profile, cb) => {
-    console.log("access token: ", accessToken);
-    console.log("refreshToken: ", refreshToken);
-    console.log("profile: ", profile);
-  }
+    (accessToken, refreshToken, profile, cb) => {
+        console.log("access token: ", accessToken);
+        console.log("refreshToken: ", refreshToken);
+        console.log("profile: ", profile);
+    }
 ));
