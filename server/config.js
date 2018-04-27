f92cecbc5f273476b9098e737881b523f2b1a192
omjadas <omja.das@gmail.com>
2018-04-27 22:56:36 +1000

    removed config.js

--- server/config.js
+++
@@ -1,9 +0,0 @@
-export const auth = {
-    FacebookID: "425379877913502",
-    GOOGLE_CLIENT_ID: "820164325496-e082jen2k1t4c28n98716nlmjkddp6og.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET: "Kcueq1EvUwC6t1JTb-2YoNrb",
-};
-
-export const db = {
-    mlab: "mongodb://bunji2018:bunji12345@ds143099.mlab.com:43099/bunji",
-}
