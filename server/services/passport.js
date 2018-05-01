09f4d46fe2bcdd069768eaaa162a6be7f1c0e68e
Terence <terrencedenning@gmail.com>
2018-05-01 22:13:15 +1000

    added cookies

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,16 @@ import { auth } from "../config";
 const mongoose = require('mongoose');
 const User = mongoose.model('user');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    });
+});
 
 passport.use(new GoogleStrategy({
     clientID: auth.GOOGLE_CLIENT_ID,
