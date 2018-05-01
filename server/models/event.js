064cfc4c1cde94f1b24250141101d2274e2ccad0
omjadas <omja.das@gmail.com>
2018-05-02 00:02:33 +1000

    added refs to event

--- server/models/event.js
+++ server/models/event.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
+import user from "./user.js";
 
 var eventSchema = mongoose.Schema({
     title: { type: String, required: true },
     date: { type: Date, required: true },
     location: { type: String, required: true },
-    user1: { type: String, required: true },
-    user2: String
+    user1: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+    user2: { type: mongoose.Schema.Types.ObjectId, ref: "user" }
 })
 
 export default mongoose.model("event", eventSchema);
