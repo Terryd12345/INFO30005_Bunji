f1dd216ba34e91df401455f9d3a133bd531cb086
omjadas <omja.das@gmail.com>
2018-05-03 23:14:48 +1000

    added routes for several functions

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,6 +7,9 @@ router.get("/user", controller.getCurrentUser);
 router.get("/user/:id", controller.getUser);
 router.post("/createUser", controller.createUser);
 
+router.post("/addSkill", controller.addSkill);
+router.post("/addConnection", controller.addConnection);
+
 router.post("/createSkill", controller.createSkill);
 router.get("/allSkills", controller.allSkills);
 
