04ede2aa4a7692b8239cf235fc9b8db131dc4a1c
omjadas <omja.das@gmail.com>
2018-05-03 23:17:43 +1000

    restructured api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 import controller from "../controllers/apiControllers";
 
+// User calls
 router.get("/user", controller.getCurrentUser);
 router.get("/user/:id", controller.getUser);
 router.post("/createUser", controller.createUser);
@@ -10,11 +11,13 @@ router.post("/createUser", controller.createUser);
 router.post("/addSkill", controller.addSkill);
 router.post("/addConnection", controller.addConnection);
 
-router.post("/createSkill", controller.createSkill);
+router.get("/chat/:id", controller.getChat);
+router.post("/chat/:id", controller.postMessage);
+
 router.get("/allSkills", controller.allSkills);
 
-router.get("/chat/:id", controller.getChat);
 
-router.post("/chat/:id", controller.postMessage);
+// Admin calls
+router.post("/createSkill", controller.createSkill);
 
 export default router;
