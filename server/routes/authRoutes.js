539d9ef34c82d1e56efb394b5681e8acedaef248
Terence <terrencedenning@gmail.com>
2018-05-06 21:51:34 +1000

    redirect to welcome

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -13,7 +13,7 @@ router.get(
     "/google/callback",
     passport.authenticate("google"),
     function (req, res) {
-        res.redirect('/dashboard')
+        res.redirect('/welcome')
     }
 );
 
