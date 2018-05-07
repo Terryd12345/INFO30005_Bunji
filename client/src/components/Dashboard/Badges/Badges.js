f3318c44ec9c7c2d0b68fba5f6c806b8fb843e03
Terence <terrencedenning@gmail.com>
2018-05-07 19:09:25 +1000

    badges refactor

--- client/src/components/Dashboard/Badges/Badges.js
+++ client/src/components/Dashboard/Badges/Badges.js
@@ -1,40 +1,24 @@
 import React, { Component } from "react";
 import ManageSkills from "./ManageSkills/ManageSkills";
+import NewEvent from './CreateEvent/NewEvent';
 
 class Badges extends Component {
-    showAvailable = () => {
-        this.manageSkills.showAvailable();
-    };
 
     render() {
         return (
             <div id="badges">
-                <ManageSkills ref={manageSkills => this.manageSkills = manageSkills} />
-
                 <div id="badges-content">
-                    <div onClick={this.showAvailable.bind(this)} className="badge centered" id="badge-1">
+                    <ManageSkills />
+                    <a href="">
+                        <NewEvent />
+                    </a>
+                    <a href="">
+                    <div className="badge centered" id="badge-3">
                         <h5>
-                            <img src={require("../../../images/icons/add.png")} alt="Icon" />
-                            Manage Skills
+                            <img src={require("../../../images/icons/find.png")} alt="Icon" />
+                            Find Mentor
                         </h5>
                     </div>
-
-                    <a href="">
-                        <div className="badge centered" id="badge-2">
-                            <h5>
-                                <img src={require("../../../images/icons/create.png")} alt="Icon" />
-                                Create an Event
-                            </h5>
-                        </div>
-                    </a>
-
-                    <a href="">
-                        <div className="badge centered" id="badge-3">
-                            <h5>
-                                <img src={require("../../../images/icons/find.png")} alt="Icon" />
-                                Find Mentor
-                            </h5>
-                        </div>
                     </a>
                 </div>
             </div>
