7bd93930a046c6512454f2bba88bd0edf29167d2
Terryd12345 <terrencedenning@gmail.com>
2018-05-09 17:18:07 +1000

    pull request

--- client/src/components/Dashboard/Badges/Badges.js
+++ client/src/components/Dashboard/Badges/Badges.js
@@ -5,12 +5,12 @@ class Badges extends Component {
     showAvailable = () => {
         this.manageSkills.showAvailable();
     };
-    
+
     render() {
         return (
             <div id="badges">
                 <ManageSkills ref={manageSkills => this.manageSkills = manageSkills} />
-                
+
                 <div id="badges-content">
                     <div onClick={this.showAvailable.bind(this)} className="badge centered" id="badge-1">
                         <h5>
@@ -18,16 +18,16 @@ class Badges extends Component {
                             Manage Skills
                         </h5>
                     </div>
-    
+
                     <a href="">
-                        <div className="badge centered" id="badge-2">
+                        <div onClick={this.showAvailable.bind(this)} className="badge centered" id="badge-2">
                             <h5>
                                 <img src={require("../../../images/icons/create.png")} alt="Icon" />
                                 Create an Event
                             </h5>
                         </div>
                     </a>
-                    
+
                     <a href="">
                         <div className="badge centered" id="badge-3">
                             <h5>
